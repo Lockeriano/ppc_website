f67aff04f7581ffd040e507bd63db0608d2f9ff4
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-03-12 16:04:08 +0100

    Add carousel, few icons and write simple script to get carousel going

--- js/main.js
+++ js/main.js
@@ -8,4 +8,34 @@ function initMap() {
     position: pos,
     map: map
   });
+};
+
+var indicator = document.getElementsByClassName('indicator'),
+    carouselQuote = document.getElementsByClassName('carousel-item');
+
+
+function indicatorSelection () {
+  if (this.className === 'indicator') {
+    for (var i = 0; i < indicator.length; i++) {
+      indicator[i].className = 'indicator'
+      carouselQuote[i].className = 'carousel-item'
+    }
+    this.className = 'indicator active'
+    for (var i = 0; i < carouselQuote.length; i++) {
+      if (indicator[0] && indicator[0].className === ('indicator active')) {
+        carouselQuote[i].className = 'carousel-item'
+        carouselQuote[0].className = 'carousel-item active'
+      } else if (indicator[1] && indicator[1].className === ('indicator active')) {
+        carouselQuote[i].className = 'carousel-item'
+        carouselQuote[1].className = 'carousel-item active'
+      } else {
+        carouselQuote[i].className = 'carousel-item'
+        carouselQuote[2].className = 'carousel-item active'
+      }
+    }
+  }
+}
+
+for (var i = 0; i < indicator.length; i++) {
+  indicator[i].addEventListener('click', indicatorSelection)
 }
